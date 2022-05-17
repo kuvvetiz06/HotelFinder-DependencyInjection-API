dadafc328e9608380858dbabc8cda0493c9cd845
HuseyiN Gokdemir <huseyingokdemir@msn.com>
2022-05-17 15:49:24 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=LAPTOP-FBSE04D5; Database=HotelDb;uid=sa;pwd=12345678;");
+        }
+        public DbSet <Hotel> Hotels{ get; set; }
+    }
+}
